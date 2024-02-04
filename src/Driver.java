5b956327aa90fc710d7d68e4423f7d4570bfe902
Mahlaki <mahlaki14@gmail.com>
2024-02-04 18:50:42 -0500

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+        Table table = new Table(2, 2);
+        table.createBoard();
+        System.out.println("SPACER");
+        table.displayBoard();
+    }
+}
